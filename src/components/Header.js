e236dfa13e820ea0d64cff53d6ef397a5a7953ee
mtrimner <matt.trimner@gmail.com>
2021-01-26 11:33:16 -0800

    made header bar render dynamically depeding on if a user is signed in or signed out

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,15 @@ import { connect } from 'react-redux';
 import Authorization from './Authorization';
 
 const Header = (props) => {
-    return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" >
+
+    const renderHeader = () => {
+        if (props.isSignedIn) {
+            return(
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" >
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="sign-in">Sign In</Nav.Link>
             <Nav.Link as={Link} to="new-diet" onClick={() => {alert("Starting a new diet will reset all current parameters. Are you sure you want to start a new diet?")}}>New Diet</Nav.Link>
         </Nav>
         <Nav >
@@ -22,11 +24,46 @@ const Header = (props) => {
         </Nav>
         </Navbar.Collapse>
         </Navbar>
+            )} else {
+                return (
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" >
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+        </Nav>
+        <Nav >
+            <Authorization />
+        </Nav>
+        </Navbar.Collapse>
+        </Navbar>
+                )}
+    }
+    return (
+        renderHeader()
+        // <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" >
+        //     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        //     <Navbar.Collapse id="responsive-navbar-nav">
+        // <Nav className="mr-auto">
+        //     <Nav.Link as={Link} to="/">Home</Nav.Link>
+        //     <Nav.Link as={Link} to="new-diet" onClick={() => {alert("Starting a new diet will reset all current parameters. Are you sure you want to start a new diet?")}}>New Diet</Nav.Link>
+        // </Nav>
+        // <Nav >
+        //     <Authorization />
+        //     <Navbar.Text className="ml-3">
+        //         Welcome <Link to="/">{props.currentUser}</Link>
+        //     </Navbar.Text>
+        // </Nav>
+        // </Navbar.Collapse>
+        // </Navbar>
     )
 }
 
 const mapStateToProps = (state) => {
-    return {currentUser: state.auth.name}
+    return {
+        currentUser: state.auth.name,
+        isSignedIn: state.auth.isSignedIn
+    }
 }
 
 export default connect(mapStateToProps)(Header);
